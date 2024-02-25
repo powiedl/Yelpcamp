b58a274fabf53513fc36aa7e1eef57db2c0d05a8
powiedl <rolandpi@gmx.at>
2024-02-25 14:08:24 +0100

    Thumbnails added

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+/* das Schema für ein Bild, notwendig für das virtual Attribut thumbnail - wird aber nicht exportiert,
+ sondern nur im CampgroundSchema verwendet - und wollte ich auch schon für die "Zerteilung" der 
+ Validierung des Dokuments vor und nach Upload machen */
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -22,6 +25,12 @@ const CampgroundSchema = new Schema({
         ref:'User'
     }
 });
+
+// Thumbnail-URL: (200x200, Seitenverhältnis beibehalten: w_200,h_200,c_limit, z. b: https://res.cloudinary.com/dqsqcmfl9/image/upload/w_200,h_200,c_limit/v1708860833/Yelpcamp/fhraqnfeq4z0ic8wj8yi.jpg
+ImageSchema.methods.getThumbnail = function(px=200) { // weil ein virtual keine Parameter unterstützt, darum eine Instanzmethode machen
+    // px Größe des thumbnails, Standardwert 200
+    return this.url.replace('/upload',`/upload/w_${px},h_${px},c_limit`);
+}
 
 // Mongoose Middleware für Campgrounds
 CampgroundSchema.post('findOneAndDelete',async function (doc) {
