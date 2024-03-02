19ba0619a3ae588a9d90edff79c66a29b34293b4
powiedl <rolandpi@gmx.at>
2024-03-02 07:04:36 +0100

    atlasDB als Datenbankprovider hinzugefügt, auch wenn am Ende der Connectionstring wieder auf localhost gestellt wird

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { match } = require('assert');
-
+const dbUrl = process.env.DB_URL;
+// mongoose.connect(dbUrl, {
 mongoose.connect('mongodb://localhost:27017/yelp-camp2', {
     useNewUrlParser: true,
     useCreateIndex: true,
